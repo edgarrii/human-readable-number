5891d1322983fbd92c5deea9d7e129ff83b9e567
edgarrii <71753826+edgarrii@users.noreply.github.com>
2021-03-15 00:25:34 +0300

    feat: add code for readable numbers

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function toReadable (number) {
-  
+  while (isNaN(number) === true ){
+    let str = number.toString();
+    let mass1_19 = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteenn', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let mass20_90 = ['', '', 'twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+    if (number == 0) return 'zero';
+    if (number < 20 && number > 0) return mass1_19[number];
+    if (number % 10 == 0 && number < 100) return mass20_90[++(str[0])];
+    if (number < 100 && number > 20 && number % 10 != 0) return mass20_90[++str[0]] + mass1_19[++(str[1])];
+    if (number % 100 == 0 && number < 1000) return mass1_19[++(str[0])] + ' hundred';
+    if (number % 10 == 0 && number % 100 != 0 && number < 1000 && number > 100) return mass1_19[++(str[0])] + ' hundred ' + mass20_90[++(str[1])];
+    if (number > 100 && number < 1000 && number % 10 != 0 && number % 100 != 0) return mass1_19[++(str[0])] + ' hundred ' + mass20_90[++(str[1])] + mass1_19[++(str[2])];
+  }
 }
